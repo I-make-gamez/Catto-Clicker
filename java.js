770d4e09cce4feaa97c99528b3834a8cf5dff3a2
I-Make-Gamez <ebelcher143@gmail.com>
2022-05-04 19:52:57 -0400

    a_01-5/4/22

---
+++ java.js
@@ -0,0 +1,12 @@
+const pbtn = document.querySelector('.p')
+const sbtn = document.querySelector('.s')
+const hbtn = document.querySelector('.h')
+const scbtn = document.querySelector('.sc')
+const pcat = document.querySelector('.pc')
+
+var top = -540
+var goal = window.inner
+
+pbtn.addEventListener('click', function(){
+setInterval()
+})
